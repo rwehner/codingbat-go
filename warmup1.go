0587d9b4a9710dc3b9c677d0af2dff3d87b5e3a3
Robert Wehner <robert.wehner@returnpath.com>
2016-11-15 16:20:25 -0700

    more work

--- warmup1.go
+++ warmup1.go
@@ -3,6 +3,8 @@
 //package warmup1
 package warmup1
 
+import "fmt"
+
 // SleepIn takes two boolean arguments (for wekday and vacation) and returns a
 // boolean. The parameter w is true if it is a weekday, and the parameter v is
 // true if we are on vacation. We sleep in if it is not a weekday or we're on
@@ -36,24 +38,46 @@ func SumDouble(a, b int) int {
 // Given an int n, return the absolute difference between n and 21, except return
 // double the absolute difference if n is over 21.
 func Diff21(n int) int {
-	return 0
+	abs := 21 - n
+	if n > 21 {
+		if abs < 0 {
+			abs = -abs
+		}
+		return abs * 2
+	}
+	return abs
 }
 
 // We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
 // We are in trouble if the parrot is talking and the hour is before 7 or after 20.
 // Return True if we are in trouble.
 func ParrotTrouble(t bool, h int) bool {
+	if t {
+		if h < 7 || h > 20 {
+			return true
+		}
+	}
 	return false
 }
 
-// Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+// Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
 func Makes10(a, b int) bool {
+	if a == 10 || b == 10 || a+b == 10 {
+		return true
+	}
 	return false
 }
 
-//Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute
-//value of a number.
+//Given an int n, return true if it is within 10 of 100 or 200.
 func NearHundred(n int) bool {
+	nums := [2]int{100, 200}
+	for _, num := range nums {
+		upper, lower := num+10, num-10
+		fmt.Println(num, upper, n, lower)
+		if n < upper && n >= lower {
+			return true
+		}
+	}
 	return false
 }
 
